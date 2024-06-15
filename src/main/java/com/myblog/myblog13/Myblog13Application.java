a196c68165a584517a112df201e27918b29dddee
twilio6203 <twilo6203@gmail.com>
2024-06-15 12:05:24 +0530

    127:Create save Post Feature

---
+++ src/main/java/com/myblog/myblog13/Myblog13Application.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog13Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myblog/myblog13/Myblog13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myblog13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
